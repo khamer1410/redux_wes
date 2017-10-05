9ff1c3ee56a4989fc201ca0a813140da867bf96d
Krzysztof Hamerszmit <krzysztof.hamerszmit@kreditech.com>
2017-10-05 17:16:25 +0200

    initial commit

---
+++ learn-redux/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Reduxstagrame
+        </h1>
+      </div>
+    )
+  }
+}
